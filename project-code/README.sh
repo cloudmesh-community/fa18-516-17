228815c086acf0dfb0117eee17b2e80ad66981e6
btpope <42694671+btpope@users.noreply.github.com>
2018-10-08 18:59:34 -0500

    Update README.sh

--- project-code/README.sh
+++ project-code/README.sh
@@ -1,6 +1,8 @@
 # git clone https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/README.sh
 # bash README.sh
 
+sudo apt-get install git-core
+
 git clone https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/bashrc-add
 
 git clone https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/pyenv-setup.sh
