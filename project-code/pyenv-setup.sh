dc4daf912303933580e7c409baa6d3e674f3ecb6
btpope <42694671+btpope@users.noreply.github.com>
2018-10-08 18:21:15 -0500

    Create pyenv-setup.sh

---
+++ project-code/pyenv-setup.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt-get install git python-pip make build-essential libssl-dev
+sudo apt-get install zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
+sudo pip install virtualenvwrapper
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv   
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+
+which pyenv
+
+pyenv install -l
+
+pyenv update
+pyenv install 2.7.15
+pyenv install 3.7.0
+
+pyenv global 3.7.0
+
+pyenv version
+
+pyenv versions
+
+pyenv virtualenv 2.7.15 ENV2
+pyenv virtualenv 3.7.0 ENV3
