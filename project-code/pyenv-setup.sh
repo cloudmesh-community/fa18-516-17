e6770eb4bae2fc8dbb106855e7fc0d90f1c00418
btpope <42694671+btpope@users.noreply.github.com>
2018-10-08 19:41:34 -0500

    Update pyenv-setup.sh

--- project-code/pyenv-setup.sh
+++ project-code/pyenv-setup.sh
@@ -16,6 +16,8 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
 
+pip install --upgrade pip
+
 source .bashrc
 
 which pyenv
