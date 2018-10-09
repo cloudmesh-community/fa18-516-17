b71b7e265544eab34139c85ddb1507a9a57f1aaa
btpope <42694671+btpope@users.noreply.github.com>
2018-10-08 19:17:20 -0500

    Update README.sh

--- project-code/README.sh
+++ project-code/README.sh
@@ -1,7 +1,7 @@
 # wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/README.sh
 # bash README.sh
 
-sudo apt-get install git-core
+sudo apt-get install -y git-core
 
 wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/bashrc-add
 
