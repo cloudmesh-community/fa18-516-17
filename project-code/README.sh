c9a0341cec1f41fbf287f8160b85001faed78427
Gregor von Laszewski <laszewski@gmail.com>
2018-10-22 19:05:30 -0400

    Update README.sh

--- project-code/README.sh
+++ project-code/README.sh
@@ -1,11 +1,3 @@
-# wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/README.sh
-# bash README.sh
-
-sudo apt-get install -y git-core
-
-wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/bashrc-add
+# setup pyenv
 
 wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/pyenv-setup.sh
-
-bash python-setup.sh
-cat bashrc-add >> .bashrc
