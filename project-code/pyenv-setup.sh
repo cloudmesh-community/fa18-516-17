99ca8094a9e172534ce5a5aaf022a32432fd33ae
Gregor von Laszewski <laszewski@gmail.com>
2018-10-22 19:04:49 -0400

    Update pyenv-setup.sh

--- project-code/pyenv-setup.sh
+++ project-code/pyenv-setup.sh
@@ -1,10 +1,13 @@
 # wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/pyenv-setup.sh
 # bash python-setup.sh
+
+sudo apt-get install -y git-core
 sudo apt-get update
 
 sudo apt-get install -y git python-pip make build-essential libssl-dev
 sudo apt-get install -y zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
 sudo pip install virtualenvwrapper
+
 
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv   
@@ -17,6 +20,8 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
 
 pip install --upgrade pip
+
+wget https://raw.githubusercontent.com/cloudmesh-community/fa18-516-17/master/project-code/bashrc-add
 
 source .bashrc
 
@@ -36,3 +41,6 @@ pyenv versions
 
 pyenv virtualenv 2.7.15 ENV2
 pyenv virtualenv 3.7.0 ENV3
+
+cat bashrc-add >> .bashrc
+source .bashrc
