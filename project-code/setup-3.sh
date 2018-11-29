7a8c5cb740c8304c9596795b3c6a072394572d78
btpope <42694671+btpope@users.noreply.github.com>
2018-11-29 16:36:51 -0600

    Create setup-3.sh

---
+++ project-code/setup-3.sh
@@ -0,0 +1,33 @@
+# Pyhive installation
+
+# Install Dependent Modules
+#Installing gcc
+
+sudo apt-get install gcc
+
+#Install Thrift
+
+pip install thrift
+
+# Install SASL
+
+sudo apt-get install libsasl2-dev
+pip install sasl
+
+# Install thrift sasl
+
+pip install thrift_sasl
+
+#Installing Pyhive
+
+pip install pyhive
+
+#copy remote.py
+
+sudo cp /home/student/project/fa18-516-17/project-code/remote.py /home/hduser/
+
+#Hive Server2 start
+
+$HIVE_HOME/bin/hiveserver2
+cd ~
+python remote.py
