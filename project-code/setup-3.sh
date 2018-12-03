7f742a3310af7e817e2ae6ad911e92761548669a
btpope <42694671+btpope@users.noreply.github.com>
2018-12-02 18:06:05 -0600

    Update setup-3.sh

--- project-code/setup-3.sh
+++ project-code/setup-3.sh
@@ -8,22 +8,17 @@ sudo apt-get install python-pip
 sudo apt-get install gcc
 
 #Install Thrift
-
 pip install thrift
 
 # Install SASL
-
 sudo apt-get install libsasl2-dev
 pip install sasl
 
 # Install thrift sasl
-
 pip install thrift_sasl
 
 #Installing Pyhive
-
 pip install pyhive
 
 #copy remote.py
-
 sudo cp /home/student/project/fa18-516-17/project-code/remote.py /home/hduser/
